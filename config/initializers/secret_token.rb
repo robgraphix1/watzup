fcf56addb5f180541eec0368e53482493cdbf584
Robert Perez <ptelerez@cox.net>
2015-04-21 19:55:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watzup::Application.config.secret_key_base = '2591547beb0536895bc27fe7dc563a5c14f2dd59efaa09f169f43ff51de83bbb2b9b7b8d077e45adc4049390fe5e20ac4042581f1ba3fe7f8edf921f81ce04a3'

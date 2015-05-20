9e16bbe2670abb6b6302a59cc1842b0de29b56b4
Robert Perez <ptelerez@cox.net>
2015-05-20 00:41:13 +0000

    built functionality to post comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Watzup::Application.routes.draw do
+ Watzup::Application.routes.draw do
   devise_for :users
   root 'places#index'
 
-  resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

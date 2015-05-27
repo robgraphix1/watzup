6667c2cf9e13aa8f6d2067f70092b7713ed46c0f
Robert Perez <ptelerez@cox.net>
2015-05-27 00:58:31 +0000

    went back and put in humanized rating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,4 +14,17 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+	RATINGS = {
+
+		'one star' => '1_star',
+		'two stars' => '2_star',
+		'three stars' => '3_star',
+		'four stars' => '4_star',
+		'five stars' => '5_star'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
 end

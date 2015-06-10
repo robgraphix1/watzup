99f6fe00004508d0d01ed423e215ac10b0c4b0b5
Robert Perez <ptelerez@cox.net>
2015-06-10 01:10:26 +0000

    made some more additions to the email feature

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,16 @@
 Watzup::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+
   config.action_mailer.default_url_options = { :host => 'localhost:3030'
 
   }
@@ -17,7 +29,7 @@ Watzup::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

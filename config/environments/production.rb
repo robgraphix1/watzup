4f126160e8fcb5d3227a0f54e11fc53fa49dcc60
Robert Perez <ptelerez@cox.net>
2015-06-13 22:29:50 +0000

    hooked up mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,16 @@ Watzup::Application.configure do
   config.action_mailer.default_url_options = {
     :host => 'watzup-rob-perez.herokuapp.com'
   }
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

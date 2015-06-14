0fc73ad67c5b3e4263863256ab6d062cd2e37bca
Robert Perez <ptelerez@cox.net>
2015-06-14 19:00:39 +0000

    started the user dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@
     resources :comments, :only => :create
     resources :photos, :only => :create
   end
-  
+   
+   resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

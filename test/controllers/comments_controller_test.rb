184e75706a892a1207dce399311bf2c9dec57f26
Robert Perez <ptelerez@cox.net>
2015-06-26 01:39:24 +0000

    added tests for Comments Controller, user controller and humanized_rating method

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comment create" do 
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+  	place = FactoryGirl.create(:place)
+  	assert_difference 'Comment.count' do
+  	post :create, :place_id => place.id, :comment => {
+  		:message => 'Great!',
+  		:rating => '5_star'
+  		}
+  
+  	end
+  	assert_redirected_to place_path(place.id)
+  	assert_equal 1, place.comments.count
+  	end
+  	
+  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "user show page" do
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+  	get :show, :id => user.id
+  	assert_response :success
+  end
+  	
+  
 end
